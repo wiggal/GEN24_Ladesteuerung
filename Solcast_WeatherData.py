09c920105977749fd969593f6407101d07fdeb5f
Ludwig Sporrer <ludwig.sporrer@gmx.de>
2023-10-16 13:03:03 +0200

    Fix: Wenn solcast.com nicht erreichbar fällt skript auf die Nase.

--- Solcast_WeatherData.py
+++ Solcast_WeatherData.py
@@ -38,39 +38,42 @@ def loadLatestWeatherData():
     #die Daten müssen 2x abgerufen werden forecasts (Zukunft) und estimated_actuals (aktuell und Vergangenheit 
     for datenloop in ["estimated_actuals","forecasts"]:
 
-        url = 'https://api.solcast.com.au/rooftop_sites/{}/{}?format=json&api_key={}'.format(resource_id, datenloop, api_key)
-        # Hier wieder ABHOLEN EIN
-        apiResponse = requests.get(url, timeout=12.50)
-        json_data1 = dict(json.loads(apiResponse.text))
-    
-        # wenn zuviele Zugriffe
         try:
-            istda = json_data1['response_status']['error_code']
-            return(dict_watts, json_data1)
-        except:
+            url = 'https://api.solcast.com.au/rooftop_sites/{}/{}?format=json&api_key={}'.format(resource_id, datenloop, api_key)
+            # Hier wieder ABHOLEN EIN
+            apiResponse = requests.get(url, timeout=12.50)
+            json_data1 = dict(json.loads(apiResponse.text))
     
-            Puffer = ['2000-01-01',5]
             try:
-                for wetterwerte in json_data1[datenloop]:
-                    key_neu_1 = (wetterwerte['period_end'][:19]).replace('T',' ',1)
-                    if ('00:00' in key_neu_1 and (heute in key_neu_1 or morgen in key_neu_1)):
-                        key_neu = datetime.strftime(datetime.strptime(key_neu_1, format) + timedelta(hours=Zeitzone), format)
-                        #hier Werte mit NULLEN weg
-                        if (wetterwerte['pv_estimate'] == 0):
-                            if (Puffer[1] == 0):
-                                Puffer = [key_neu, int(wetterwerte['pv_estimate']*1000*KW_Faktor)]
-                                continue
+                # wenn zuviele Zugriffe
+                istda = json_data1['response_status']['error_code']
+                return(dict_watts, json_data1)
+            except:
+        
+                Puffer = ['2000-01-01',5]
+                try:
+                    for wetterwerte in json_data1[datenloop]:
+                        key_neu_1 = (wetterwerte['period_end'][:19]).replace('T',' ',1)
+                        if ('00:00' in key_neu_1 and (heute in key_neu_1 or morgen in key_neu_1)):
+                            key_neu = datetime.strftime(datetime.strptime(key_neu_1, format) + timedelta(hours=Zeitzone), format)
+                            #hier Werte mit NULLEN weg
+                            if (wetterwerte['pv_estimate'] == 0):
+                                if (Puffer[1] == 0):
+                                    Puffer = [key_neu, int(wetterwerte['pv_estimate']*1000*KW_Faktor)]
+                                    continue
+                                else:
+                                    Puffer = [key_neu, int(wetterwerte['pv_estimate']*1000*KW_Faktor)]
+                                    dict_watts['result']['watts'][key_neu] = int(wetterwerte['pv_estimate']*1000*KW_Faktor)
+                                    continue
                             else:
+                                if (Puffer[1] == 0):
+                                    dict_watts['result']['watts'][Puffer[0]] = Puffer[1]
                                 Puffer = [key_neu, int(wetterwerte['pv_estimate']*1000*KW_Faktor)]
                                 dict_watts['result']['watts'][key_neu] = int(wetterwerte['pv_estimate']*1000*KW_Faktor)
-                                continue
-                        else:
-                            if (Puffer[1] == 0):
-                                dict_watts['result']['watts'][Puffer[0]] = Puffer[1]
-                            Puffer = [key_neu, int(wetterwerte['pv_estimate']*1000*KW_Faktor)]
-                            dict_watts['result']['watts'][key_neu] = int(wetterwerte['pv_estimate']*1000*KW_Faktor)
-            except:
-                continue
+                except:
+                    continue
+        except requests.exceptions.RequestException as error:
+            json_data1 = error
 
     
     #solcast_2.json Nun wieder nach Datum und Stunden sortieren
