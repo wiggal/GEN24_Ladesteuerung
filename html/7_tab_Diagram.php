8b5b2ff88acbe59c2d62fa42ee9df3f989a3975d
Ludwig Sporrer <ludwig.sporrer@gmx.de>
2023-11-22 07:49:41 +0100

    Auswahloptionen hinzugefügt

--- html/7_tab_Diagram.php
+++ html/7_tab_Diagram.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
     <head>
+    <title>Diagramme</title>
     <script src="chart.js"></script>
     <style>
     html, body {
@@ -11,6 +12,20 @@
         height: 100%;
     }
     .navi {
+    cursor:pointer;
+    color:#000000;
+    font-family:Arial;
+    font-size: 150%;
+    padding:6px 11px;
+  }
+    .optionwahl {
+    cursor:pointer;
+    color:#000000;
+    font-family:Arial;
+    font-size: 200%;
+    padding:6px 11px;
+  }
+    .date {
     cursor:pointer;
     color:#000000;
     font-family:Arial;
@@ -41,58 +56,54 @@ if (!file_exists($SQLite_file)) {
     echo "</body></html>";
     exit();
 }
+#print_r($_POST);
 
 ## BEGIN FUNCTIONS
-function schalter_ausgeben ( $case, $nextcase , $heute, $DiaTag, $Tag_davor, $Tag_danach, $AC_Produktion, $buttoncolor, $mengencolor, $button_vor_on, $button_back_on)
+function schalter_ausgeben ( $energietype, $nextenergietype , $heute, $morgen, $DiaDatenVon, $DiaDatenBis, $VOR_DiaDatenVon, $VOR_DiaDatenBis, $NACH_DiaDatenVon, $NACH_DiaDatenBis, $Produktion, $buttoncolor, $mengencolor, $button_vor_on, $button_back_on)
 {
 
+$schaltername = explode(" ", $DiaDatenVon);
 # Schalter zum Blättern usw.
 echo '<table><tr><td>';
 echo '<form method="POST" action="'.$_SERVER["PHP_SELF"].'">'."\n";
-echo '<input type="hidden" name="DiaTag" value="'.$Tag_davor.'">'."\n";
-echo '<input type="hidden" name="case" value="'.$case.'">'."\n";
+echo '<input type="hidden" name="DiaDatenVon" value="'.$VOR_DiaDatenVon.'">'."\n";
+echo '<input type="hidden" name="DiaDatenBis" value="'.$VOR_DiaDatenBis.'">'."\n";
+echo '<input type="hidden" name="energietype" value="'.$energietype.'">'."\n";
 echo '<button type="submit" class="navi" '.$button_back_on.'> &nbsp;&lt;&lt;&nbsp;</button>';
 echo '</form>'."\n";
 
 echo '</td><td>';
 echo '<form method="POST" action="'.$_SERVER["PHP_SELF"].'">'."\n";
-echo '<input type="hidden" name="DiaTag" value="'.$heute.'">'."\n";
-echo '<input type="hidden" name="case" value="'.$case.'">'."\n";
-echo '<button type="submit" class="navi"> '.$DiaTag.' </button>';
+echo '<input type="hidden" name="DiaDatenVon" value="'.$heute.'">'."\n";
+echo '<input type="hidden" name="DiaDatenBis" value="'.$morgen.'">'."\n";
+echo '<input type="hidden" name="energietype" value="'.$energietype.'">'."\n";
+echo '<button type="submit" class="navi"> '.$schaltername[0].' </button>';
 echo '</form>'."\n";
 
 echo '</td><td>';
 echo '<form method="POST" action="'.$_SERVER["PHP_SELF"].'">'."\n";
-echo '<input type="hidden" name="DiaTag" value="'.$Tag_danach.'">'."\n";
-echo '<input type="hidden" name="case" value="'.$case.'">'."\n";
+echo '<input type="hidden" name="DiaDatenVon" value="'.$NACH_DiaDatenVon.'">'."\n";
+echo '<input type="hidden" name="DiaDatenBis" value="'.$NACH_DiaDatenBis.'">'."\n";
+echo '<input type="hidden" name="energietype" value="'.$energietype.'">'."\n";
 echo '<button type="submit" class="navi" '.$button_vor_on.'> &nbsp;&gt;&gt;&nbsp; </button>';
 echo '</form>'."\n";
 
 echo '</td><td>';
 echo '<form method="POST" action="'.$_SERVER["PHP_SELF"].'">'."\n";
 echo '<input type="hidden" name="DiaTag" value="'.$DiaTag.'">'."\n";
-echo '<input type="hidden" name="case" value="'.$nextcase.'">'."\n";
-echo '<button type="submit" class="navi" style="background-color:'.$buttoncolor.'"> '.$nextcase.'&gt;&gt; </button>';
+echo '<input type="hidden" name="energietype" value="'.$nextenergietype.'">'."\n";
+echo '<button type="submit" class="navi" style="background-color:'.$buttoncolor.'"> '.$nextenergietype.'&gt;&gt; </button>';
 echo '</form>'."\n";
 
-# Hier noch die Zweiträume einbinden
-/*
-echo '</td><td>';
+echo '</td><td style="text-align:center; width: 100%;">';
 echo '<form method="POST" action="'.$_SERVER["PHP_SELF"].'">'."\n";
-echo '<input type="hidden" name="DiaTag" value="'.$DiaTag.'">'."\n";
-echo '<input type="hidden" name="case" value="'.$nextcase.'">'."\n";
-echo '<select name="top5">';
-echo '<option>Tag</option>';
-echo '<option>Monat</option>';
-echo '<option>Jahr</option>';
-echo '<option>gesamt</option>';
-echo '</select>';
+echo '<input type="hidden" name="DiaTag" value="'.$Tag_danach.'">'."\n";
+echo '<input type="hidden" name="energietype" value="option">'."\n";
+echo '<button type="submit" class="navi" > Optionen </button>';
 echo '</form>'."\n";
-*/
 
-echo '</td><td style="text-align:right; width: 100%; font-size: 170%">';
 echo '</td><td style="text-align:right; font-size: 170%; background-color: '.$mengencolor.'">';
-echo "&nbsp;$AC_Produktion KWh&nbsp;";
+echo "&nbsp;$Produktion KWh&nbsp;";
 
 echo '</td></tr></table><br>';
 }
@@ -132,11 +143,17 @@ return array($daten, $labels);
 $db = new SQLite3($SQLite_file);
 
 # Diagrammtag festlegen
-$heute = date("Y-m-d");
-$DiaTag = $heute;
-if (isset($_POST["DiaTag"])) $DiaTag = $_POST["DiaTag"];
-$Tag_davor = date("Y-m-d",(strtotime("-1 day", strtotime($DiaTag))));
-$Tag_danach = date("Y-m-d",(strtotime("+1 day", strtotime($DiaTag))));
+$heute = date("Y-m-d 00:00");
+$morgen =  date("Y-m-d 00:00",(strtotime("+1 day", strtotime(date("Y-m-d")))));
+$DiaDatenVon = $heute;
+$DiaDatenBis = $morgen;
+#$DiaTag = $heute;
+if (isset($_POST["DiaDatenVon"])) $DiaDatenVon = str_replace("T"," ",$_POST["DiaDatenVon"]);
+if (isset($_POST["DiaDatenBis"])) $DiaDatenBis = str_replace("T"," ",$_POST["DiaDatenBis"]);
+$VOR_DiaDatenVon = date("Y-m-d 00:00",(strtotime("-1 day", strtotime($DiaDatenVon))));
+$VOR_DiaDatenBis = date("Y-m-d 00:00",(strtotime("-1 day", strtotime($DiaDatenBis))));
+$NACH_DiaDatenVon = date("Y-m-d 00:00",(strtotime("+1 day", strtotime($DiaDatenVon))));
+$NACH_DiaDatenBis = date("Y-m-d 00:00",(strtotime("+1 day", strtotime($DiaDatenBis))));
 # Schalter für morgen deaktivieren
 $button_vor_on = '';
 if ($DiaTag == $heute) $button_vor_on = 'disabled';
@@ -145,23 +162,23 @@ $button_back_on = '';
 $DBersterTag = (explode(" ",$db->querySingle('SELECT MIN(Zeitpunkt) from pv_daten')));
 if ($DiaTag == $DBersterTag[0]) $button_back_on = 'disabled';
 
-# case = Verbrauch oder Produktion
-$case = 'Produktion';
-if (isset($_POST["case"])) $case = $_POST["case"];
+# energietype = Verbrauch oder Produktion
+$energietype = 'Produktion';
+if (isset($_POST["energietype"])) $energietype = $_POST["energietype"];
 
 # switch Verbrauch oder Produktion
-switch ($case) {
+switch ($energietype) {
     case 'Produktion':
 
 # AC Produktion 
 $SQL = "SELECT 
         MAX(DC_Produktion)- MIN(DC_Produktion)
         AS DC_Produktion
-from pv_daten where Zeitpunkt LIKE '".$DiaTag."%'";
+from pv_daten where Zeitpunkt BETWEEN '".$DiaDatenVon."' AND '".$DiaDatenBis."'";
 $DC_Produktion = round($db->querySingle($SQL)/1000, 1);
 
 # Funtkion Schalter aufrufen
-schalter_ausgeben('Produktion', 'Verbrauch', $heute, $DiaTag, $Tag_davor, $Tag_danach, $DC_Produktion, 'red', 'rgb(34,139,34)', $button_vor_on, $button_back_on);
+schalter_ausgeben('Produktion', 'Verbrauch', $heute, $morgen, $DiaDatenVon, $DiaDatenBis, $VOR_DiaDatenVon, $VOR_DiaDatenBis, $NACH_DiaDatenVon, $NACH_DiaDatenBis, $DC_Produktion, 'rgb(255,158,158)', 'rgb(0,255,127)', $button_vor_on, $button_back_on);
 
 # ProduktionsSQL
 # Aussieben der manchmal entstehenden Minuswerte im Verbrauch durch "AND Gesamtverbrauch > 1"
@@ -175,7 +192,7 @@ $SQL = "WITH Alle_PVDaten AS (
         ((Batterie_IN - LAG(Batterie_IN) OVER(ORDER BY Zeitpunkt)) - (Batterie_OUT - LAG(Batterie_OUT) OVER(ORDER BY Zeitpunkt))) AS InBatterie,
         Vorhersage,
         BattStatus
-from pv_daten where Zeitpunkt LIKE '".$DiaTag."%')
+from pv_daten where Zeitpunkt BETWEEN '".$DiaDatenVon."' AND '".$DiaDatenBis."')
 SELECT Zeitpunkt,
     Direktverbrauch*60/ROUND((JULIANDAY(Zeitpunkt) - JULIANDAY(LAG(Zeitpunkt) OVER(ORDER BY Zeitpunkt))) * 1440) AS Direktverbrauch,
     Gesamtverbrauch*60/ROUND((JULIANDAY(Zeitpunkt) - JULIANDAY(LAG(Zeitpunkt) OVER(ORDER BY Zeitpunkt))) * 1440) AS Gesamtverbrauch,
@@ -188,7 +205,7 @@ Where Zeitabstand > 4 AND Gesamtverbrauch > 1";
 
 $results = $db->query($SQL);
 
-# Funktion diagrammdaten
+# Funktion Diagrammdaten
 $DB_Werte = array('Gesamtverbrauch', 'Einspeisung', 'InBatterie', 'Direktverbrauch');
 list($daten, $labels) = diagrammdaten($results, $DB_Werte);
 
@@ -210,11 +227,11 @@ $SQL = "SELECT
         MAX(AC_Produktion) - min(AC_Produktion) + 
         MIN (Einspeisung) - MAX (Einspeisung)
         AS AC_Produktion
-from pv_daten where Zeitpunkt LIKE '".$DiaTag."%'";
+from pv_daten where Zeitpunkt BETWEEN '".$heute."' AND '".$morgen."'";
 $AC_Verbrauch = round($db->querySingle($SQL)/1000, 1);
 
 # Schalter aufrufen
-schalter_ausgeben('Verbrauch', 'Produktion', $heute, $DiaTag, $Tag_davor, $Tag_danach, $AC_Verbrauch, 'rgb(34,139,34)', 'red', $button_vor_on, $button_back_on);
+schalter_ausgeben('Verbrauch', 'Produktion', $heute, $morgen, $DiaDatenVon, $DiaDatenBis, $VOR_DiaDatenVon, $VOR_DiaDatenBis, $NACH_DiaDatenVon, $NACH_DiaDatenBis, $AC_Verbrauch, 'rgb(0,255,127)', 'rgb(255,158,158)', $button_vor_on, $button_back_on);
 
 # VerbrauchSQL
 # Aussieben der manchmal entstehenden Minuswerte im Verbrauch durch "AND Gesamtverbrauch > 1"
@@ -227,7 +244,7 @@ $SQL = "WITH Alle_PVDaten AS (
         (DC_Produktion - LAG(DC_Produktion) OVER(ORDER BY Zeitpunkt)) AS Produktion,
         ((Batterie_OUT - LAG(Batterie_OUT) OVER(ORDER BY Zeitpunkt)) - (Batterie_IN - LAG(Batterie_IN) OVER(ORDER BY Zeitpunkt))) AS VonBatterie,
         BattStatus
-from pv_daten where Zeitpunkt LIKE '".$DiaTag."%')
+from pv_daten where Zeitpunkt BETWEEN '".$heute."' AND '".$morgen."')
 SELECT Zeitpunkt,
     Direktverbrauch*60/ROUND((JULIANDAY(Zeitpunkt) - JULIANDAY(LAG(Zeitpunkt) OVER(ORDER BY Zeitpunkt))) * 1440) AS Direktverbrauch,
     Produktion*60/ROUND((JULIANDAY(Zeitpunkt) - JULIANDAY(LAG(Zeitpunkt) OVER(ORDER BY Zeitpunkt))) * 1440) AS Produktion,
@@ -253,18 +270,122 @@ $optionen['Direktverbrauch'] = ['Farbe' => 'rgba(255,215,0,1)', 'fill' => 'true'
     break; # ENDE case Verbrauch
     
 } # ENDE switch
+if ($energietype == 'option') {
+echo "
+<center>
+<br><p class='optionwahl'>Diagrammoptionen auswählen!</p><br><br>
+</center>
+";
+# Hier Auswahllisten 
+echo "
+<script type='text/javascript'>
+function zeitsetzer(offset) {
+const date = new Date();
+
+let day_von = date.getDate();
+let day_bis = date.getDate();
+let month_von = date.getMonth() + 1;
+let month_bis = date.getMonth() + 1;
+let year_von = date.getFullYear();
+let year_bis = date.getFullYear();
+let hours = '0';
+let minutes_html = '0' - date.getTimezoneOffset();
+
+// 1 = Tag
+if (offset == 1) {
+  day_bis  = day_bis + 1;
+  document.getElementsByName('diagramtype')[0].checked = true;
+}
+
+// 2 = Monat
+if (offset == 2) {
+  day_von  = 1;
+  day_bis  = 1;
+  month_bis = month_bis + 1;
+  document.getElementsByName('diagramtype')[1].checked = true;
+}
+
+// 3 = Monat
+if (offset == 3) {
+  day_von  = 1;
+  day_bis  = 1;
+  month_von = 1;
+  month_bis = 1;
+  year_bis = year_bis + 1;
+  document.getElementsByName('diagramtype')[1].checked = true;
+}
+
+
+let von = year_von + '-' + ('0'+month_von).substr(-2) + '-' + ('0'+day_von).substr(-2) + 'T00:00:00';
+//alert(von);
+let bis = year_bis + '-' + ('0'+month_bis).substr(-2)+ '-' + ('0'+day_bis).substr(-2) + 'T00:00:00';
+//alert(bis);
+document.getElementById('DiaDatenVon').value = von;
+document.getElementById('DiaDatenBis').value = bis;
+}
+window.onload = function() { zeitsetzer(1); };
+</script>
+
+<div style='text-align: center;'>
+<form method='POST' action='$_SERVER[PHP_SELF]'>
+  <fieldset style='display: inline-block;'>
+  <legend class='optionwahl'>Energieart:</legend>
+  <div style='text-align: left'>
+  <input style='text-align: left' type='radio' id='Produktion' name='energietype' value='Produktion' checked>
+  <label class='optionwahl' for='Produktion'>Produktion</label><br>
+  <input type='radio' id='Verbrauch' name='energietype' value='Verbrauch'>
+  <label class='optionwahl' for='Verbrauch'>Verbrauch</label><br>
+  </div>
+  </fieldset>
+  <br>
+
+  <fieldset style='display: inline-block;'>
+  <legend class='optionwahl'>Diagrammart:</legend>
+  <div style='text-align: left'>
+  <input type='radio' id='line' name='diagramtype' value='line' checked>
+  <label class='optionwahl' for='line'>Linien</label><br>
+  <input type='radio' id='bar' name='diagramtype' value='bar'>
+  <label class='optionwahl' for='bar'>Balken&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label><br>
+  </div>
+  </fieldset>
+  <br>
+
+  <fieldset style='display: inline-block;'>
+  <legend class='optionwahl'>Zeitraum:</legend>
+  <div style='text-align: left'>
+  <label class='optionwahl' >Von:&nbsp;</label>
+  <input class='date' type='datetime-local' name='DiaDatenVon' id='DiaDatenVon' value='' /><br><br>
+  <label class='optionwahl' >Bis:&nbsp;&nbsp;&nbsp;</label>
+  <input class='date' type='datetime-local' name='DiaDatenBis' id='DiaDatenBis' value='' /><br><br>
+  <input type='radio' id='tag' name='Zeitraum' value='tag' onclick='zeitsetzer(1)' checked>
+  <label class='optionwahl' for='tag'>Tag&nbsp;&nbsp;&nbsp;&nbsp;</label>
+  <input type='radio' id='monat' name='Zeitraum' value='monat' onclick='zeitsetzer(2)'>
+  <label class='optionwahl' for='monat'>Monat</label>
+  <input type='radio' id='jahr' name='Zeitraum' value='jahr' onclick='zeitsetzer(3)'>
+  <label class='optionwahl' for='jahr'>Jahr</label>
+  </div>
+  </fieldset>
+
+<br><br>
+<button type='submit' class='navi' > Diagramm aufrufen</button>
+</form>
+</div>
+<b>Noch nicht umgesetzt:</b><br>
+- Blättern in den Zeitreihen<br>
+- Balkendiagramme<br>
+";
+} else {
+# Wenn Optionen dann kein Chart
 $db->close();
-?>
-<div class="container">
-  <canvas id="PVDaten" style="height:100vh; width:100vw"></canvas>
+echo "<div class='container'>
+  <canvas id='PVDaten' style='height:100vh; width:100vw'></canvas>
 </div>
 <script>
-new Chart("PVDaten", {
+new Chart('PVDaten', {
     type: 'line',
     data: {
-      labels: [<?php echo $labels; ?>],
-      datasets: [{
-<?php
+      labels: [". $labels ."],
+      datasets: [{";
       $trenner = "";
       foreach($daten as $x => $val) {
       echo $trenner;
@@ -282,8 +403,7 @@ new Chart("PVDaten", {
       echo "yAxisID: '".$optionen[$x]['yAxisID']."'\n";
       $trenner = "},{\n";
       }
-?>
-    }]
+echo "    }]
     },
     options: {
       responsive: true,
@@ -373,7 +493,9 @@ new Chart("PVDaten", {
     }
 },
   });
-</script>
+</script>";
+}
+?>
 
     </body>
 </html>
