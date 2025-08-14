8970e1b91ed144e1443efb2194a6d730649217aa
Ludwig Sporrer <ludwig.sporrer@gmx.de>
2025-08-14 10:12:50 +0200

    Alte Funktion aufgeräumt

--- FUNCTIONS/Steuerdaten.py
+++ FUNCTIONS/Steuerdaten.py
@@ -6,19 +6,6 @@ sqlall = FUNCTIONS.SQLall.sqlall()
 
 class readcontroldata:
 
-    def loadPVReservierung(self, controlfile):
-        self.controlfile = controlfile
-        reservierungdata = None
-        with open(self.controlfile) as json_file:
-            reservierungdata = json.load(json_file)
-        try:
-            with open(self.controlfile) as json_file:
-                reservierungdata = json.load(json_file)
-        except:
-                print("ERROR: Reservierungsdatei fehlt, bitte erzeugen oder Option abschalten !!")
-                exit()
-        return reservierungdata
-
     def getParameter(self, argv, schluessel):
         Parameter = ""
         Meldung = ""
@@ -58,4 +45,3 @@ class readcontroldata:
                 Options = Optionen
 
         return(Parameter, Meldung, Options)
-    
