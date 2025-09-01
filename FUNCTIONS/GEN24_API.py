2534ce1f6ca19f0827766ea9b947315a2d8cb9b8
Ludwig Sporrer <ludwig.sporrer@gmx.de>
2025-09-01 18:25:24 +0200

    Summenbildung bei Mehrfachschlüssel nun in der Schlüsselsuchfunktion

--- FUNCTIONS/GEN24_API.py
+++ FUNCTIONS/GEN24_API.py
@@ -1,6 +1,7 @@
 import FUNCTIONS.functions
 import requests
 import json
+import re
         
 basics = FUNCTIONS.functions.basics()
 sqlall = FUNCTIONS.SQLall.sqlall()
@@ -9,7 +10,7 @@ class gen24api:
     def __init__(self):
         self.dummy = 'dummmy'
 
-    def extract_API_values(self, obj, keys):
+    def old_extract_API_values(self, obj, keys):
         """
         Durchsucht obj nach den Schlüsseln aus 'keys' und gibt ein dict zurück.
         """
@@ -28,6 +29,53 @@ class gen24api:
         recurse(obj)
         return results
 
+    def extract_API_values(self, obj, key_patterns):
+        """
+        Extrahiert nur Keys aus obj, die in key_patterns definiert sind.
+        Berechnet Summen für Keys mit sum_flag=True.
+        key_patterns: Liste von Tuples (pattern, sum_flag)
+            - pattern: Regex oder exakter Key
+            - sum_flag: True = Werte summieren, False = einzeln zurückgeben
+    
+        Rückgabe: dict mit allen Keys aus key_patterns und Summen für markierte Keys.
+        """
+        results = {}
+        sums = {}
+
+        # Summen-Namen genau aus Pattern erzeugen oder individuell anpassen
+        sum_names = {}
+        for pattern, sum_flag in key_patterns:
+            if sum_flag:
+                # Beispiel: SUM_<Pattern> ohne Regex-Zeichen
+                # [1-3] bzw. [1-2] entfernen
+                clean_pattern = re.sub(r'\[\d+-\d+\]', '', pattern)
+                # Sonderzeichen durch '_' ersetzen
+                sum_name = "SUM_" + re.sub(r'[^A-Za-z0-9_]', '_', clean_pattern)
+                sums[sum_name] = 0
+                sum_names[pattern] = sum_name
+
+        def recurse(o):
+            if isinstance(o, dict):
+                for k, v in o.items():
+                    for pattern, sum_flag in key_patterns:
+                        if re.fullmatch(pattern, k):
+                            if sum_flag:
+                                sums[sum_names[pattern]] += v
+                            else:
+                                results[k] = v
+                            break  # Key wurde verarbeitet, nicht weiter prüfen
+                    if isinstance(v, (dict, list)):
+                        recurse(v)
+            elif isinstance(o, list):
+                for item in o:
+                    recurse(item)
+
+        recurse(obj)
+        results.update(sums)
+        return results
+
+
+
     # API-Werte lesen unabhängig von den Node-Nummern
     def get_API(self):
         IP = basics.getVarConf('gen24','hostNameOrIp','str')
@@ -35,34 +83,33 @@ class gen24api:
         url = requests.get(gen24url)
         data = json.loads(url.text)
         API = {}
-        # "393216 OR 0 -  channels - BAT_MODE_ENFORCED_U16" : 2.0, AKKU AUS
-        # "393216 OR 0 -  channels - BAT_MODE_ENFORCED_U16" : 0.0, AKKU EIN
-        # relevante API-Schlüssel definieren
+        # relevante API-Schlüssel definieren: 
+        # schluessel [1-3] bei mehrfachschlussel, True für Summenbildung
         GEN24_API_schluessel = [
-            "BAT_MODE_ENFORCED_U16",
-            "nameplate",
-            "BAT_VALUE_STATE_OF_HEALTH_RELATIVE_U16",
-            "BAT_VALUE_STATE_OF_CHARGE_RELATIVE_U16",
-            "SMARTMETER_POWERACTIVE_MEAN_SUM_F64",
-            "PV_POWERACTIVE_MEAN_01_F32",
-            "PV_POWERACTIVE_MEAN_02_F32",
-            "BAT_POWERACTIVE_MEAN_F32",
-            "ACBRIDGE_ENERGYACTIVE_PRODUCED_SUM_01_U64",
-            "ACBRIDGE_ENERGYACTIVE_PRODUCED_SUM_02_U64",
-            "ACBRIDGE_ENERGYACTIVE_PRODUCED_SUM_03_U64",
-            "PV_ENERGYACTIVE_ACTIVE_SUM_01_U64",
-            "PV_ENERGYACTIVE_ACTIVE_SUM_02_U64",
-            "ACBRIDGE_ENERGYACTIVE_ACTIVECONSUMED_SUM_01_U64",
-            "ACBRIDGE_ENERGYACTIVE_ACTIVECONSUMED_SUM_02_U64",
-            "ACBRIDGE_ENERGYACTIVE_ACTIVECONSUMED_SUM_03_U64",
-            "BAT_ENERGYACTIVE_ACTIVECHARGE_SUM_01_U64",
-            "BAT_ENERGYACTIVE_ACTIVEDISCHARGE_SUM_01_U64",
-            "SMARTMETER_ENERGYACTIVE_CONSUMED_SUM_F64",
-            "SMARTMETER_ENERGYACTIVE_PRODUCED_SUM_F64"
+            ("BAT_MODE_ENFORCED_U16", False),
+            ("nameplate", False),
+            ("BAT_VALUE_STATE_OF_HEALTH_RELATIVE_U16", False),
+            ("BAT_VALUE_STATE_OF_CHARGE_RELATIVE_U16", False),
+            ("SMARTMETER_POWERACTIVE_MEAN_SUM_F64", False),
+            ("PV_POWERACTIVE_MEAN_01_F32", False),
+            ("PV_POWERACTIVE_MEAN_02_F32", False),
+            ("BAT_POWERACTIVE_MEAN_F32", False),
+            ("ACBRIDGE_ENERGYACTIVE_PRODUCED_SUM_0[1-3]_U64", True),
+            ("PV_ENERGYACTIVE_ACTIVE_SUM_0[1-2]_U64", True),
+            ("ACBRIDGE_ENERGYACTIVE_ACTIVECONSUMED_SUM_0[1-3]_U64", True),
+            ("BAT_ENERGYACTIVE_ACTIVECHARGE_SUM_01_U64", False),
+            ("BAT_ENERGYACTIVE_ACTIVEDISCHARGE_SUM_01_U64", False),
+            ("SMARTMETER_ENERGYACTIVE_CONSUMED_SUM_F64", False),
+            ("SMARTMETER_ENERGYACTIVE_PRODUCED_SUM_F64", False)
         ]
         API_result = self.extract_API_values(data, GEN24_API_schluessel)
 
+        #print(API_result)  #entWIGGlung
+        #exit()  #entWIGGlung
+
         API['BAT_MODE'] = API_result['BAT_MODE_ENFORCED_U16']
+        # "BAT_MODE_ENFORCED_U16" : 2.0, AKKU AUS
+        # "BAT_MODE_ENFORCED_U16" : 0.0, AKKU EIN
         if API['BAT_MODE'] != 2:
             # Benötigte Werte mit den geholten API-Werten rechnen und API zuweisen
             # Aktuelle Werte für Prognoseberechung
@@ -81,12 +128,9 @@ class gen24api:
             API['aktuellePVProduktion'] = int(API_result['PV_POWERACTIVE_MEAN_01_F32'] + API_STRING2)
             API['aktuelleBatteriePower'] = int(API_result['BAT_POWERACTIVE_MEAN_F32'])
             # Zählerstände fürs Logging
-            API['AC_Produktion'] =  int((API_result['ACBRIDGE_ENERGYACTIVE_PRODUCED_SUM_01_U64']+API_result['ACBRIDGE_ENERGYACTIVE_PRODUCED_SUM_02_U64']+\
-                                         API_result['ACBRIDGE_ENERGYACTIVE_PRODUCED_SUM_03_U64'])/3600)
-            API['DC_Produktion'] =  int((API_result['PV_ENERGYACTIVE_ACTIVE_SUM_01_U64']+ API_result['PV_ENERGYACTIVE_ACTIVE_SUM_02_U64'])/3600)
-            API['AC_to_DC'] = int((API_result['ACBRIDGE_ENERGYACTIVE_ACTIVECONSUMED_SUM_01_U64'] +\
-                                   API_result['ACBRIDGE_ENERGYACTIVE_ACTIVECONSUMED_SUM_02_U64'] +\
-                                   API_result['ACBRIDGE_ENERGYACTIVE_ACTIVECONSUMED_SUM_03_U64'])/3600)
+            API['AC_Produktion'] =  int(API_result['SUM_ACBRIDGE_ENERGYACTIVE_PRODUCED_SUM_0_U64']/3600)
+            API['DC_Produktion'] =  int(API_result['SUM_PV_ENERGYACTIVE_ACTIVE_SUM_0_U64']/3600)
+            API['AC_to_DC'] =       int(API_result['SUM_ACBRIDGE_ENERGYACTIVE_ACTIVECONSUMED_SUM_0_U64']/3600)
             API['Batterie_IN'] =    int(API_result['BAT_ENERGYACTIVE_ACTIVECHARGE_SUM_01_U64']/3600)
             API['Batterie_OUT'] =   int(API_result['BAT_ENERGYACTIVE_ACTIVEDISCHARGE_SUM_01_U64']/3600)
             API['Netzverbrauch'] =  int(API_result['SMARTMETER_ENERGYACTIVE_CONSUMED_SUM_F64'])
@@ -109,15 +153,13 @@ class gen24api:
                         API_STRING2 = API_result['PV_POWERACTIVE_MEAN_02_F32']
                     except:
                         API_STRING2 = 0
-                    API['aktuellePVProduktion'] += int( API_result['PV_POWERACTIVE_MEAN_01_F32'] + API_STRING2)
-                    API['AC_Produktion']        += int((API_result['ACBRIDGE_ENERGYACTIVE_PRODUCED_SUM_01_U64']+API_result['ACBRIDGE_ENERGYACTIVE_PRODUCED_SUM_02_U64']+\
-                                                        API_result['ACBRIDGE_ENERGYACTIVE_PRODUCED_SUM_03_U64'])/3600)
-                    API['DC_Produktion']        += int((API_result['PV_ENERGYACTIVE_ACTIVE_SUM_01_U64']+ API_result['PV_ENERGYACTIVE_ACTIVE_SUM_02_U64'])/3600)
-                    API['AC_to_DC']             += int((API_result['ACBRIDGE_ENERGYACTIVE_ACTIVECONSUMED_SUM_01_U64'] +\
-                                                        API_result['ACBRIDGE_ENERGYACTIVE_ACTIVECONSUMED_SUM_02_U64'] +\
-                                                        API_result['ACBRIDGE_ENERGYACTIVE_ACTIVECONSUMED_SUM_03_U64'])/3600)
-                except:
-                    print("API von WR ", weitereIP, " nicht erreichbar")
+                    API['aktuellePVProduktion'] += int(API_result['PV_POWERACTIVE_MEAN_01_F32'] + API_STRING2)
+                    API['AC_Produktion']        += int(API_result['SUM_ACBRIDGE_ENERGYACTIVE_PRODUCED_SUM_0_U64']/3600)
+                    API['DC_Produktion']        += int(API_result['SUM_PV_ENERGYACTIVE_ACTIVE_SUM_0_U64']/3600)
+                    API['AC_to_DC']             += int(API_result['SUM_ACBRIDGE_ENERGYACTIVE_ACTIVECONSUMED_SUM_0_U64']/3600)
+                except Exception as e:
+                    print("API von WR", weitereIP, "nicht erreichbar")
+                    print("Fehlerursache:", e)
 
         # Daten von Symos lesen und addieren
         IP_weitere_Symo = basics.getVarConf('gen24','IP_weitere_Symo','str')
@@ -125,8 +167,8 @@ class gen24api:
             IP_weitere_Symo = IP_weitere_Symo.replace(" ", "")
             IP_weitere_Symo = IP_weitere_Symo.split(",")
             SYMO_API_schluessel = [
-                "PowerReal_PAC_Sum",
-                "EnergyReal_WAC_Sum_EverSince"
+                ("PowerReal_PAC_Sum", False),
+                ("EnergyReal_WAC_Sum_EverSince", False)
             ]
             API_Sym = {}
             API_Sym['aktuellePVProduktion'] = 0
